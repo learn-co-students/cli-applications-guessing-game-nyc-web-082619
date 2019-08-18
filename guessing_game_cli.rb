2afd380bd1c36bcbe64406f00081caf3ee1bbc4d
Kevin Karma <kevinkarma55@gmail.com>
2019-08-18 03:41:41 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,11 @@
-# Code your solution here!
+def run_guessing_game
+  num = rand 1..6
+  input = gets.chomp
+  if input == num
+    puts "You guessed the corrent number!"
+  elsif input == "exit"
+    puts "Goodbye!"
+  else 
+    puts "Sorry! The computer guessed " + num.to_s + "."
+  end
+end
